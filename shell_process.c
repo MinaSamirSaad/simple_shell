43f03be8d2f78ccdb7426290beea9a6954ddf9ad
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-05-25 21:06:57 +0300

    some updates

--- shell_process.c
+++ shell_process.c
@@ -12,7 +12,7 @@
 int sh(char *str, char **args, char **paths, int p_cnt, char *p_path)
 {
 int status = 1;
-char *breaks = " ";
+char *breaks = " \n";
 char *clean_string;
 str = get_string();
 /*check EOF*/
